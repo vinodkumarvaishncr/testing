d9d2ce48f5d5ee6b71a4d8cd8d2acf498f547a06
unknown <vinodkumarvaishncr@gmail.com>
2025-06-30 19:54:42 +0530

    all add

---
+++ provide.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  subscription_id = "0fb3b41d-23bc-47d2-861e-1582e1789bd5"
+  features {}
+}
