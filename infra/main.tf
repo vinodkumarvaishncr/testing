ee9437bbaff0009086febe37e042b7bcd15ccce5
VINODKUMAR\Vinod Kumar <vinodkumarvaishncr@gmail.com>
2025-08-17 14:56:55 +0530

    'sfsf'

---
+++ infra/main.tf
@@ -0,0 +1,10 @@
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-vinod2-testing"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "rg-vinod22-testing"
+  location = "West Europe"
+}
---
+++ infra/provide.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+ 
+ backend "azurerm" {
+      resource_group_name  = "vinod-main"
+      storage_account_name = "vinodmainstorage"
+      container_name       = "vinodconatiner"
+      key                  = "vinodkey.tfstate"
+  }
+
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  subscription_id = "96a3eaff-d4cf-4222-9fe7-1622ab46f13e"
+  features {}
+  
+}
