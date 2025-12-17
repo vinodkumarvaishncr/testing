7d3a217bdd8dd66cee17dbc9c52bec0b61a3fcc6
vinodkumarvaishncr <vinodkumarvaishncr@gmail.com>
2025-12-17 14:45:15 +0530

    Rename Azure resource groups for consistency

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,10 @@
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-vinod1_test1"
+  name     = "rg-vinod1_test11"
   location = "canada central"
 }
 
 resource "azurerm_resource_group" "rg2" {
-  name     = "rg-vinod2-test2"
+  name     = "rg-vinod2-test22"
   location = "canada central"
 }
